787b3965fbac361eb0e3e805133e4b55ca13a0e5
Jasper Liu <41106000+jliu0316@users.noreply.github.com>
2022-03-09 20:46:22 -0800

    Update football.py

--- football.py
+++ football.py
@@ -16,8 +16,6 @@ This app does webscraping of NFL Football player rushing data
 st.sidebar.header('Categories')
 selected_year = st.sidebar.selectbox('Year', list(reversed(range(1990,2022))))
 
-# Web scraping of NFL player stats
-# https://www.pro-football-reference.com/years/2019/rushing.htm
 @st.cache
 def load_data(year):
     url = "https://www.pro-football-reference.com/years/" + str(year) + "/rushing.htm"
@@ -29,22 +27,19 @@ def load_data(year):
     return playerstats
 playerstats = load_data(selected_year)
 
-# Sidebar - Team selection
 sorted_unique_team = sorted(playerstats.Tm.unique())
 selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
 
-# Sidebar - Position selection
 unique_pos = ['RB','QB','WR','FB','TE']
 selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
 
-# Filtering data
 df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
 
 st.header('Display Player Stats of Selected Team(s)')
 st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
 st.dataframe(df_selected_team)
 
-#csv download
+#csv 
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  
@@ -54,8 +49,8 @@ def filedownload(df):
 st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
 
 # Heatmap
-if st.button('Intercorrelation Heatmap'):
-    st.header('Intercorrelation Matrix Heatmap')
+if st.button('Heatmap'):
+    st.header('Heatmap')
     df_selected_team.to_csv('output.csv',index=False)
     df = pd.read_csv('output.csv')
 
